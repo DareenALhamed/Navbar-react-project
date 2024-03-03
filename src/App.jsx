06470ff48523d5d5c9df631c0c327d99e032a5bf
Dareen Alhamed <alhamed.dareen@gmail.com>
2024-03-03 21:25:32 +0200

    navbar-react

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import Navbar from "./Navbar.jsx";
+import { useEffect } from "react";
+
+
+function App() {
+ 
+ 
+  return (
+    <>
+      <Navbar />
+      
+    </>
+  );
+}
+
+export default App;
